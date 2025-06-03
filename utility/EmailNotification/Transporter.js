4737ab2aed37c9f37277f8f9c8cf987d7f9d4468
Anubhav <1rn21cs026.anubhavkumar@rnsit.ac.in>
2025-06-03 12:28:01 +0530

    update transporter code

--- utility/EmailNotification/Transporter.js
+++ utility/EmailNotification/Transporter.js
@@ -1,8 +1,7 @@
 const dotenv = require("dotenv");
 const nodemailer = require("nodemailer");
 dotenv.config();
-console.log("MAIL_USER:", process.env.MAIL_USER);
-console.log("MAIL_PASSWORD:", process.env.MAIL_PASSWORD);
+
 // Set up the transporter for sending email
 const transporter = nodemailer.createTransport({
   host: 'smtp.titan.email',
